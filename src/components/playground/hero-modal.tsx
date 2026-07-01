fe4f6cbbdc2bb654c576b320d4a7272db52440e5
Duong Vu <vu.t.duong01@gmail.com>
2026-07-01 08:36:49 -0400

    Put hero modal items into a grid

--- src/components/playground/hero-modal.tsx
+++ src/components/playground/hero-modal.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { ReactElement } from "react";
 
 interface HeroModalProps {
   id: string,
@@ -9,8 +10,32 @@ interface HeroModalProps {
 }
 
 const HeroModal = ({ id, name, icon, items, onItemClicked }: HeroModalProps) => {
+  const itemButton = (item: string) => {
+    if (!item || item === "") return <div></div>
+    return (
+      <button type="button" className="btn btn-secondary shadow-lg m-1" title={`${name} item`} onClick={() => onItemClicked(item)}>
+        <Image className="img-fluid" src={item} alt={`${name} item`} width={200} height={200} loading="lazy" />
+      </button>
+    );
+  }
+
+  const itemGrid = () => {
+    let rows: ReactElement[] = [];
+    for (let i = 0; i < items.length; i += 2) {
+      const item1 = items[i];
+      const item2 = items[i + 1];
+      rows = [...rows,
+      <div className="row" key={`itemRow${i}`}>
+        <div className="col">{itemButton(item1)}</div>
+        <div className="col">{itemButton(item2)}</div>
+      </div>
+      ]
+    }
+    return rows;
+  }
+
   return (
-    <div className="modal fade" id={id} tabIndex={-1} aria-labelledby={`${id}ModalLabel`}  data-bs-backdrop="static">
+    <div className="modal fade" id={id} tabIndex={-1} aria-labelledby={`${id}ModalLabel`}>
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header bg-body-tertiary">
@@ -19,16 +44,7 @@ const HeroModal = ({ id, name, icon, items, onItemClicked }: HeroModalProps) => 
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            {items.map((item) => {
-              return (
-                <div key={item}>
-                  <button type="button" className="btn btn-secondary shadow-lg m-1" title={`${name} item`} onClick={() => onItemClicked(item)}>
-                    <Image className="img-fluid w-100" src={item} alt={`${name} item`} width={200} height={200} loading="lazy" />
-                  </button>
-                  <br />
-                </div>
-              );
-            })}
+            {itemGrid()}
           </div>
           <div className="modal-footer">
             <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Done <i className="bi bi-check-circle-fill ms-1"></i></button>
